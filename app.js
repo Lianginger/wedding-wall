3c7faf5401ce39fe1833458d6ef5c1f7e45e9634
Lianginger <jackyliang444@hotmail.com.tw>
2019-10-24 10:41:26 +0800

    Add post / request

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Wedding wall!!!')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.json({
+    status: 'ok'
+  })
 })
 
 app.listen(port, () => {
