06fe923d5c193133972c815b20e848dc931e86e8
Lianginger <jackyliang444@hotmail.com.tw>
2019-10-27 23:55:24 +0800

    Update card schema

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ db.once('open', function() {
 })
 
 const Card = mongoose.model('Card', {
+  name: String,
   image: String,
-  text: String
+  text: String,
+  userId: Number,
+  profilePicURL: String
 })
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -29,8 +32,11 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
   console.log(req.body)
   const newCard = new Card({
+    name: req.body.name,
     image: req.body.image,
-    text: req.body.text
+    text: req.body.text,
+    userId: req.body.userId,
+    profilePicURL: req.body.profilePicURL
   })
   await newCard.save()
   res.json({
