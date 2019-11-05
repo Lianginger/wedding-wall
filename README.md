45b7461a91c37b84167016b09e98ccfe44ab8da4
Lianginger <jackyliang444@hotmail.com.tw>
2019-11-06 02:01:13 +0800

    Set sharp package to handle image resize and compress

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Wedding Wall 婚禮祝福牆
 
 為了哥哥 11 及 12 月份的訂婚、結婚，結合 Messenger 聊天機器人，打造一個與來賓互動的及時婚禮祝福牆。
+
+Images are resized and compress with sharp
+
+裁減圖片大小考量到主要需求，依據 slideshow 會用投影機，投影機常見檢析度：
+
+- 1920 x 1080
+- 1280 x 768
+- 800 x 600
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const Datauri = require('datauri')
 const imagemin = require('imagemin')
 const imageminJpegtran = require('imagemin-jpegtran')
 const imageminPngquant = require('imagemin-pngquant')
+const sharp = require('sharp')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -37,25 +38,65 @@ module.exports = app => {
   })
 
   app.post('/cards/new', upload.single('image'), async (req, res) => {
-    const datauri = new Datauri()
-    // 裁減大小
-    console.time('imagemin')
-    const compressedImageBuffer = await imagemin.buffer(req.file.buffer, {
-      plugins: [
-        imageminJpegtran(),
-        imageminPngquant({
-          quality: [0.6, 0.8]
-        })
-      ]
-    })
-    console.timeEnd('imagemin')
+    if (!req.file || !req.body.name || !req.body.text) {
+      const errorMessage = {
+        status: 'error',
+        title: '每個欄位都要填寫喔！',
+        text: '請確認填寫姓名、祝福語及照片。'
+      }
+      res.render('new', { errorMessage })
+    }
+
+    // 檢查檔案大小
+    const max_img_size = 2097152
+    if (req.file.size > max_img_size) {
+      const errorMessage = {
+        status: 'error',
+        title: '喔！圖片太大了',
+        text: '圖片大小必須小於 2 MB，請重新選擇一張照片。'
+      }
+      res.render('new', { errorMessage })
+    }
+
+    // 尺寸大於 1280x768 調整尺寸大小
+    console.time('croppedImage-sharp')
+    const image = sharp(req.file.buffer)
+    let imageMetadata = await image.metadata()
+    let croppedImage = req.file.buffer
+    let isResize = 0
+    if (imageMetadata.height > 768) {
+      isResize++
+      croppedImage = await image
+        .resize({ height: 768 })
+        .toFormat('jpeg')
+        .toBuffer()
+    }
+    imageMetadata = await sharp(croppedImage).metadata()
+    if (imageMetadata.width > 1280) {
+      isResize++
+      croppedImage = await sharp(croppedImage)
+        .resize({ width: 1280 })
+        .toFormat('jpeg')
+        .toBuffer()
+    }
+    if (isResize === 0) {
+      croppedImage = await image.toFormat('jpeg').toBuffer()
+    }
+    console.timeEnd('croppedImage-sharp')
+
+    // 調整大小前後差多少
+    console.log('isResize', isResize)
+    console.log('原始圖片大小', req.file.size)
+    const croppedImageMetadata = await sharp(croppedImage).metadata()
+    console.log('調整後圖片大小', croppedImageMetadata.size)
+    console.log('壓縮率', ((req.file.size - croppedImageMetadata.size) * 100) / req.file.size, '%')
 
     console.time('datauri')
-    await datauri.format(path.extname(req.file.originalname).toString(), compressedImageBuffer)
+    const datauri = new Datauri()
+    await datauri.format(path.extname(req.file.originalname).toString(), croppedImage)
     console.timeEnd('datauri')
     const file = datauri.content
 
-    // 上傳前檢查檔案大小 1m，太大就返回警告
     console.time('cloudinary')
     const result = await cloudinary.uploader.upload(file, function(error, result) {
       console.log(result, error)
